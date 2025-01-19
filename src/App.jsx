46b9704293f027471bc59869087e813f9727b245
rvrutan <rutanroni@gmail.com>
2025-01-19 16:49:57 -0500

    tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Navigation from './components/Navigation'
+import Navigation from './components/Navbar'
 
 
 function App() {
 
   return (
     <>
+    <title>Roni v. Rutan</title>
     <Header />
     <Navigation />
     <Footer />
