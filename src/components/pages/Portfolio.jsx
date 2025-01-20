8f6bf0dc3e67d1d114945e300a9e5228a8710667
rvrutan <rutanroni@gmail.com>
2025-01-19 19:10:25 -0500

    added more projects

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,4 +1,4 @@
-import Project from '../Project';
+import Project from "../Project";
 
 export default function Portfolio() {
   return (
@@ -6,17 +6,29 @@ export default function Portfolio() {
       <h2 className="text-3xl font-bold text-center mb-4">My Projects</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {/* Repeat this Project component for each project */}
-        <Project 
-          title="Weather Dashboard" 
-          img="/assets/weaather-dashboard demo.png" 
-          liveLink="https://weather-dashboard-9vqe.onrender.com/" 
-          repoLink="https://github.com/rvrutan/weather-dashboard" 
-        />
-        <Project 
-          title="JUMPER" 
-          img="assets/jumper.png" 
+        <Project
+          title="JUMPER"
+          img="assets/jumper.png"
           liveLink="https://rvrutan.github.io/jumper/"
-          repoLink="https://github.com/rvrutan/jumper" 
+          repoLink="https://github.com/rvrutan/jumper"
+        />
+        <Project
+          title="Weather Dashboard"
+          img="/assets/weaather-dashboard demo.png"
+          liveLink="https://weather-dashboard-9vqe.onrender.com/"
+          repoLink="https://github.com/rvrutan/weather-dashboard"
+        />
+        <Project
+          title="README Generator"
+          img="assets/readme-gerator.JPG"
+          liveLink=""
+          repoLink="https://github.com/rvrutan/readme-generator"
+        />
+        <Project
+          title="Employee Tracker"
+          img="assets/employee-tracker.JPG"
+          liveLink=""
+          repoLink="https://github.com/rvrutan/employee-tracker"
         />
       </div>
     </section>
--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -3,6 +3,13 @@ export default function Resume() {
       <section className="text-center m-10">
         <h2 className="text-3xl font-bold mb-4 ">Resume</h2>
         <a href="assets/Roni Rutan Resume.pdf" download className="bg-blue-700 text-white p-2 rounded">Download Resume</a>
+        <h3 className="text-2xl font-bold mt-6">Proficiencies</h3>
+      <ul className="list-disc list-inside">
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>HTML & CSS</li>
+      </ul>
       </section>
     );
   }
